6dde1ad656fb2eb7b18bb03d74a50a48283b11cb
NTabakharniuk <98890833+NTabakharniuk@users.noreply.github.com>
2022-11-24 16:16:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,82 @@ function binarySearch(array, num) {
 }
 
 console.log(`Index: ${binarySearch(arr, 5)}`);
+
+//Пошук об'єкту за віком. Масив відсортований за віком//
+let arrPersons = [
+  { name: 'Jack', age: 18 },
+  { name: 'Mark', age: 30 },
+  { name: 'David', age: 35 },
+  { name: 'Karl', age: 41 },
+];
+
+arrPersons.sort(function (a, b) {
+  if (a.age > b.age) {
+    return 1;
+  }
+  if (a.age < b.age) {
+    return -1;
+  }
+  if (a.age == b.age) {
+    return 0;
+  }
+});
+console.log(arrPersons);
+let sourch = function (arr, num) {
+  let left = 0;
+  let right = arr.length - 1;
+  let mid;
+  while (left <= right) {
+    mid = Math.round((right - left) / 2) + left;
+    if (num === arr[mid].age) {
+      return mid;
+    } else if (num < arr[mid].age) {
+      right = mid - 1;
+    } else if (num > arr[mid].age) {
+      left = mid + 1;
+    }
+  }
+  return -1;
+};
+console.log(`Number object: ${sourch(arrPersons, 41)}`);
+
+
+
+//Пошук об'єкту за віком. Масив невідсортований за віком//
+let arrPersons = [
+  { name: 'Jack', age: 48 },
+  { name: 'Mark', age: 18 },
+  { name: 'David', age: 35 },
+  { name: 'Karl', age: 21 },
+];
+
+arrPersons.sort(function (a, b) {
+  if (a.age > b.age) {
+    return 1;
+  }
+  if (a.age < b.age) {
+    return -1;
+  }
+  if (a.age == b.age) {
+    return 0;
+  }
+});
+console.log(arrPersons);
+let sourch = function (arr, num) {
+  let left = 0;
+  let right = arr.length - 1;
+  let mid;
+  while (left <= right) {
+    mid = Math.round((right - left) / 2) + left;
+    if (num === arr[mid].age) {
+      return mid;
+    } else if (num < arr[mid].age) {
+      right = mid - 1;
+    } else if (num > arr[mid].age) {
+      left = mid + 1;
+    }
+  }
+  return -1;
+};
+console.log(`Number object: ${sourch(arrPersons, 21)}`);
+
